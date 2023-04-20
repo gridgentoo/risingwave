a55815a6b2794480badee9528d224fcf2a1a7b29
zwang28 <84491488@qq.com>
2023-04-20 22:55:15 +0800

    add sanity check in list_version

--- src/ctl/src/cmd_impl/hummock/list_version.rs
+++ src/ctl/src/cmd_impl/hummock/list_version.rs
@@ -12,7 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use risingwave_pb::hummock::{PinnedSnapshotsSummary, PinnedVersionsSummary};
+use risingwave_hummock_sdk::key_range::KeyRangeCommon;
+use risingwave_pb::hummock::{
+    HummockVersion, Level, LevelType, PinnedSnapshotsSummary, PinnedVersionsSummary,
+};
 use risingwave_rpc_client::HummockMetaClient;
 
 use crate::CtlContext;
@@ -21,7 +24,70 @@ pub async fn list_version(context: &CtlContext) -> anyhow::Result<()> {
     let meta_client = context.meta_client().await?;
     let version = meta_client.get_current_version().await?;
     println!("{:#?}", version);
+    sanity_check(&version);
     Ok(())
+}
+
+fn sanity_check(hummock_version: &HummockVersion) {
+    for levels in hummock_version.levels.values() {
+        for level in &levels.l0.as_ref().unwrap().sub_levels {
+            sanity_check_level(format!("{}-{}", 0, level.level_idx), level);
+        }
+        for level in &levels.levels {
+            sanity_check_level(format!("{}", level.level_idx), level);
+        }
+    }
+    println!("sanity check done");
+}
+
+fn sanity_check_level(level_idx: String, level: &Level) {
+    use risingwave_hummock_sdk::prost_key_range::KeyRangeExt;
+    use risingwave_hummock_sdk::KeyComparator;
+    if level.level_type() == LevelType::Overlapping {
+        return;
+    }
+    for ii in 0..level.table_infos.len() {
+        let left_sst = &level.table_infos[ii];
+        for i in 0..ii {
+            let left_left_sst = &level.table_infos[i];
+            if left_left_sst.key_range.as_ref().unwrap().sstable_overlap(&left_sst.key_range.as_ref().unwrap()) {
+                tracing::error!(
+                    "E0 level {} invalid SSTs. left-left: {:#?} left: {:#?}",
+                    level_idx,
+                    left_left_sst,
+                    left_sst,
+                );
+            }
+            if left_left_sst.key_range.as_ref().unwrap().full_key_overlap(&left_sst.key_range.as_ref().unwrap()) {
+                tracing::error!(
+                    "E1 level {} invalid SSTs. left-left: {:#?} left: {:#?}",
+                    level_idx,
+                    left_left_sst,
+                    left_sst,
+                );
+            }
+        }
+        if ii + 1 == level.table_infos.len() {
+            continue;
+        }
+        let right_sst = &level.table_infos[ii + 1];
+        if left_sst.key_range.as_ref().unwrap().compare(right_sst.key_range.as_ref().unwrap()) != std::cmp::Ordering::Less  {
+            tracing::error!(
+                "E2 level {} invalid SSTs. left: {:#?} right: {:#?}",
+                level_idx,
+                left_sst,
+                right_sst,
+            );
+        }
+        if KeyComparator::compare_encoded_full_key(&left_sst.key_range.as_ref().unwrap().right, &right_sst.key_range.as_ref().unwrap().left) != std::cmp::Ordering::Less {
+            tracing::error!(
+                "E3 level {} invalid SSTs. left: {:#?} right: {:#?}",
+                level_idx,
+                left_sst,
+                right_sst,
+            );
+        }
+    }
 }
 
 pub async fn list_pinned_versions(context: &CtlContext) -> anyhow::Result<()> {
